fa1d7a30a59f30fac30cb7bd7e924b5dde39715f
sreya <sreya@SREYANTH>
2026-04-16 12:49:15 +0530

    Add project files.

---
+++ Name/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Please enter your name:");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello, {name}!");
---
+++ addtwonums/addtwonums/Program.cs
@@ -0,0 +1,4 @@
+﻿int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sum =num1+ num2;    
+Console.WriteLine(sum);
